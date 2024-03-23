f684909cb5459b201e2e274d0b6adb21b5381c3d
Mahammed Sarfaraz Nawaz Pagala <72764237+pmdnawaz1@users.noreply.github.com>
2024-03-23 15:35:42 +0530

    .

--- server/config.js
+++ server/config.js
@@ -1,5 +1,1 @@
-module.exports = {
-  PORT: process.env.PORT || 5000,
-  DB_URL: process.env.DB_URL || 'mongodb+srv://pmdnawaz1:Yarasool12@cluster0.vbrmysz.mongodb.net/login?retryWrites=true&w=majority',
-  SECRET_KEY: process.env.SECRET_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDEwZTY5ZDU2MjM4MDQzNWJhMzU4YjQiLCJpYXQiOjE2Nzg4MjkzMzYsImV4cCI6MTY3OTQzNDEzNn0.8WiorVBYO4Vh8VkKxawlwZDb_ysbaGq-ffN4Gt3k2lI',
-};
+
